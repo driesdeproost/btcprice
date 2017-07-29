e8ea718184155e7df1bd607e26e7fa6175bbbe96
Dries De Proost <driesdeproost@gmail.com>
2017-07-29 13:31:22 +0200

    add ',' to regex to deal with prices like 2,320.05

--- btcprice.sh
+++ btcprice.sh
@@ -1,1 +1,1 @@
-curl -s http://api.coindesk.com/v1/bpi/currentprice/EUR.json | grep -o '"EUR","rate":"[0-9.]*"' | cut -d'"' -f6 | sed s'/..$//'
+curl -s http://api.coindesk.com/v1/bpi/currentprice/EUR.json | grep -o '"EUR","rate":"[0-9,.]*"' | cut -d'"' -f6 | sed s'/..$//'
